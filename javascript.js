b833f368a76db2e8a9976a892a117afab6f1c88f
user.email <developerjenzii@gmail.com>
2022-08-23 19:16:47 +0200

    Add method to add book and examples

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let bookInfo;
+let myLibrary = [];
+
 function Book(title, author, numberOfPages, readBook) {
   this.title = title;
   this.author = author;
@@ -14,5 +17,26 @@ function Book(title, author, numberOfPages, readBook) {
   }
 }
 
-const deVS = new Book('De verenigde staten in de 20e eeuw', 'Maarten van Rossum', '509', true);
-console.log(deVS.info());
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+// function removeBookFromLibrary(book) {
+//   for ( bookItem in myLibrary ) {
+//     if ( bookItem.numberOfPages === this.book ) {
+//       myLibrary = myLibrary.splice(bookItem, 1);
+//       console.log(myLibrary); 
+//     }
+//   }
+// }
+
+const book1 = new Book('De verenigde staten in de 20e eeuw', 'Maarten van Rossum', '509', true);
+addBookToLibrary(book1);
+
+const book2 = new Book('Pluk van de Petteflet', 'Annie M.G. Schmidt', '200', true);
+addBookToLibrary(book2);
+
+const book3 = new Book('De antwoorden op de Grote Vragen', 'Stephen Hawking', '264', false);
+addBookToLibrary(book3);
+console.log(myLibrary);
+
