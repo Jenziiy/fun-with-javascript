fb1b7dfaf235c98dd5412a54f9150432ac4402b4
user.email <developerjenzii@gmail.com>
2022-08-27 01:16:52 +0200

    Add new books to library

--- javascript.js
+++ javascript.js
@@ -6,7 +6,21 @@ const divBookStore = document.querySelector('.bookstore');
 addEventListener('DOMContentLoaded', removeBookFromLibrary);
 const submit = document.querySelector('#submit-button');
 submit.addEventListener('click', () => {document.querySelector('#add-book').style.display='none';
-document.querySelector('.grid').classList.toggle('greyedout')});
+document.querySelector('.grid').classList.toggle('greyedout');});
+submit.addEventListener('click', () => {const form = document.forms[0];
+  const selectAuthor = form.querySelector('input[name="author"]');
+  let selectedAuthor = selectAuthor.value;
+  const selectTitle = form.querySelector('input[name="Title"]');
+  let selectedTitle = selectTitle.value;
+  const selectPages = form.querySelector('input[name="pagesnumber"]');
+  let selectedPages = selectPages.value;
+  const selectHasRead = form.querySelector('input[name="hasread"]');
+  let selectedHasRead= selectHasRead.value;
+  const book4 = new Book(selectedAuthor, selectedTitle, selectedPages, selectedHasRead);
+  addBookToLibrary(book4);
+  displayBook();
+// displayBookStore()
+});
 const addBookButton = document.querySelector('.add-book');
 addBookButton.addEventListener('click', () => {document.querySelector('#add-book').style.display='flex'; 
 document.querySelector('.grid').classList.add('greyedout')});
@@ -71,6 +85,20 @@ function displayBookStore(){
     return buttons;
   })}
 
+function displayBook(){
+  const book = myLibrary.at(-1)
+  const button = document.createElement('button');
+    button.classList.add(`remove`); button.setAttribute('data',`${myLibrary.indexOf(book)}`);
+    const divBook = document.createElement('div')
+    divBook.classList.add('book');
+    divBook.setAttribute('id',`${myLibrary.indexOf(book)}`);
+    divBookStore.appendChild(divBook);
+    const para = document.createElement('p');
+    para.appendChild(document.createTextNode(book.info()));
+    divBook.appendChild(para);
+    divBook.appendChild(button);
+    return buttons;
+  }
 const book1 = new Book('De verenigde staten in de 20e eeuw', 'Maarten van Rossum', '509', true);
 addBookToLibrary(book1);
 
