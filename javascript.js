6d81b80ba2319c70413d037d45afcb69a64bb893
user.email <developerjenzii@gmail.com>
2022-08-27 18:01:58 +0200

    Remove books and code cleanup

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 let bookInfo;
 let myLibrary = [];
-let buttons;
-// const divBook = document.querySelector('.book');
-const divBookStore = document.querySelector('.bookstore');
-addEventListener('DOMContentLoaded', removeBookFromLibrary);
+let myLibraryShadow = [];
+let removeButtons;
+let books;
+
+let divBookStore = document.querySelector('.bookstore');
+addEventListener('DOMContentLoaded', () => removeBookFromLibrary(10));
+// divBookStore.addEventListener('mouseover', () => removeBookFromLibrary(10));
 const submit = document.querySelector('#submit-button');
 submit.addEventListener('click', () => {document.querySelector('#add-book').style.display='none';
 document.querySelector('.grid').classList.toggle('greyedout');});
@@ -18,15 +21,18 @@ submit.addEventListener('click', () => {const form = document.forms[0];
   let selectedHasRead= selectHasRead.value;
   const book4 = new Book(selectedAuthor, selectedTitle, selectedPages, selectedHasRead);
   addBookToLibrary(book4);
-  displayBook();
-  removeBookFromLibrary();
+  const books = document.querySelectorAll('.book');
+  books.forEach(book => book.remove());
+
+  displayBookStore();
 });
 const addBookButton = document.querySelector('.add-book');
 addBookButton.addEventListener('click', () => {document.querySelector('#add-book').style.display='flex'; 
 document.querySelector('.grid').classList.add('greyedout')});
 
+function getBooks() { return books = document.querySelectorAll('.book')};
 
-function idCreator() { if (myLibrary == '') { return id = 0} else {return id = myLibrary.length} };
+function idCreator() { if (myLibraryShadow == '') { return id = 0} else {return id = myLibraryShadow.length} };
 
 
 function Book(title, author, numberOfPages, readBook) {
@@ -51,54 +57,57 @@ function Book(title, author, numberOfPages, readBook) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  myLibraryShadow.push(book);
 }
 
-function removeBookFromLibrary() {
-  const buttons =  document.querySelectorAll('.remove'); 
-  buttons.forEach(button => {button.addEventListener('click', (e) => {
-    let removeBook = button.getAttribute('data');
-    for ( bookItem of myLibrary ) {
-      if ( bookItem.id == button.getAttribute('data') ) {
-        myLibrary.splice(bookItem.id, 1);
-        let removeBookItem = document.getElementById(removeBook);
-        removeBookItem.remove();}
-        else if ( myLibrary.length == 1) { document.querySelector('.book').remove();}
-        console.log(myLibrary);
-    }
+// function removeBookFromLibrary() {
+//   const buttons =  document.querySelectorAll('.remove'); 
+//   let removeBook;
+//   buttons.forEach(button => {button.addEventListener('click', (e) => {
+//     removeBook = button.getAttribute('data');
+//     for ( bookItem of myLibrary ) {
+//       if ( bookItem.id == removeBook ) {
+//         myLibrary.splice(bookItem.id, 1);
+//         let removeBookItem = document.querySelector(`[id='${removeBook}']`);
+//         removeBookItem.remove();}
+//         else if ( myLibrary.length == 1) { document.querySelector('.book').remove();}
+//         console.log(myLibrary);
+//     }
 
-  })})
-  
-}
+//   })})}
 
 function displayBookStore(){
   myLibrary.forEach((book) => {  
     const button = document.createElement('button');
-    button.classList.add(`remove`); button.setAttribute('data',`${myLibrary.indexOf(book)}`);
+    button.classList.add(`remove`); button.setAttribute('data',`${myLibraryShadow.indexOf(book)}`);
     const divBook = document.createElement('div')
-    divBook.classList.add('book');
-    divBook.setAttribute('id',`${myLibrary.indexOf(book)}`);
+    divBook.classList.add('book'); divBook.setAttribute('data',`${myLibraryShadow.indexOf(book)}`);
+    divBook.setAttribute('id',`${myLibraryShadow.indexOf(book)}`);
     divBookStore.appendChild(divBook);
     const para = document.createElement('p');
     para.appendChild(document.createTextNode(book.info()));
     divBook.appendChild(para);
     divBook.appendChild(button);
-    return buttons;
+    removeButtons =  document.querySelectorAll('.remove'); 
+    getBooks();
+    books.forEach(b => b.addEventListener('click', () => removeBookFromLibrary(b.id)));
   })}
 
-function displayBook(){
-  const book = myLibrary.at(-1)
-  const button = document.createElement('button');
-    button.classList.add(`remove`); button.setAttribute('data',`${myLibrary.indexOf(book)}`);
-    const divBook = document.createElement('div')
-    divBook.classList.add('book');
-    divBook.setAttribute('id',`${myLibrary.indexOf(book)}`);
-    divBookStore.appendChild(divBook);
-    const para = document.createElement('p');
-    para.appendChild(document.createTextNode(book.info()));
-    divBook.appendChild(para);
-    divBook.appendChild(button);
-    return buttons;
-  }
+// function displayBook(){
+//   const book = myLibrary.at(-1)
+//   const button = document.createElement('button');
+//     button.classList.add(`remove`); button.setAttribute('data',`${myLibraryShadow.indexOf(book)}`);
+//     const divBook = document.createElement('div')
+//     divBook.classList.add('book'); divBook.setAttribute('data',`${myLibraryShadow.indexOf(book)}`);
+//     divBook.setAttribute('id',`${myLibraryShadow.indexOf(book)}`);
+//     divBookStore.appendChild(divBook);
+//     const para = document.createElement('p');
+//     para.appendChild(document.createTextNode(book.info()));
+//     divBook.appendChild(para);
+//     divBook.appendChild(button);
+//     removeButtons =  document.querySelectorAll('.remove'); 
+//     return removeButtons;
+//   }
 const book1 = new Book('De verenigde staten in de 20e eeuw', 'Maarten van Rossum', '509', true);
 addBookToLibrary(book1);
 
@@ -107,9 +116,20 @@ addBookToLibrary(book2);
 
 const book3 = new Book('De antwoorden op de Grote Vragen', 'Stephen Hawking', '264', false);
 addBookToLibrary(book3);
-console.log(myLibrary);
+console.log("initial lol");
 displayBookStore();
 
+console.log(books)
+removeButtons =  document.querySelectorAll('.remove'); 
+removeButtons.forEach(button => {button.addEventListener('click', (e) => removeBookFromLibrary(e.target.getAttribute('data')  )) })
 
 
-
+function removeBookFromLibrary(id) {
+  removeButtons =  document.querySelectorAll('.remove'); 
+  for ( let book of myLibrary ) {
+    if ( book.id == id )  {myLibrary.splice(myLibrary.indexOf(book), 1);
+  document.querySelectorAll(`[data='${id}']`).forEach(book => book.remove());}
+  }
+  console.log(myLibrary);
+  return removeButtons;
+}
