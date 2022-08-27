b6d1716a52d1a3538e0ee5de73f358aa619e0311
user.email <developerjenzii@gmail.com>
2022-08-27 21:28:49 +0200

    Bug swap author and title in object

--- javascript.js
+++ javascript.js
@@ -6,14 +6,6 @@ const addBookButton = document.querySelector('.add-book');
 const submit = document.querySelector('#submit-button');
 
 
-function getBooks() { 
-  return books = document.querySelectorAll('.book')
-};
-
-function generateBookID() {
-   if (myLibraryShadow == '') { return id = 0} else {return id = myLibraryShadow.length} 
-};
-
 function Book(title, author, numberOfPages, readBook) {
   this.id = generateBookID();
   this.title = title;
@@ -29,20 +21,24 @@ function Book(title, author, numberOfPages, readBook) {
     return `${title} by ${author}, ${numberOfPages} pages, ${readBook} `;
   }
 }
-
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-  myLibraryShadow.push(book);
-  return myLibrary;
+function getBooks() { 
+  return books = document.querySelectorAll('.book')
 }
-
-function removeBookFromLibrary(id) {
-  for ( let book of myLibrary ) {
-    if ( book.id == id )  {myLibrary.splice(myLibrary.indexOf(book), 1);
-  document.querySelectorAll(`[data='${id}']`).forEach(book => book.remove());}
-  }
+function generateBookID() {
+   if (myLibraryShadow == '') { return id = 0} else {return id = myLibraryShadow.length} 
 }
+function loadInitialLibrary() {
+  const book1 = new Book('De verenigde staten in de 20e eeuw', 'Maarten van Rossum', '509', true);
+  addBookToLibrary(book1);
+  
+  const book2 = new Book('Pluk van de Petteflet', 'Annie M.G. Schmidt', '200', true);
+  addBookToLibrary(book2);
+  
+  const book3 = new Book('De antwoorden op de Grote Vragen', 'Stephen Hawking', '264', false);
+  addBookToLibrary(book3);
 
+  displayBookStore();
+}
 function displayBookStore(){
   myLibrary.forEach((book) => {  
     const button = document.createElement('button');
@@ -58,20 +54,32 @@ function displayBookStore(){
     getBooks();
     books.forEach(b => b.addEventListener('click', () => removeBookFromLibrary(b.id)));})
 }
-
-function loadInitialLibrary() {
-  const book1 = new Book('De verenigde staten in de 20e eeuw', 'Maarten van Rossum', '509', true);
-  addBookToLibrary(book1);
-  
-  const book2 = new Book('Pluk van de Petteflet', 'Annie M.G. Schmidt', '200', true);
-  addBookToLibrary(book2);
-  
-  const book3 = new Book('De antwoorden op de Grote Vragen', 'Stephen Hawking', '264', false);
-  addBookToLibrary(book3);
-
+function addBook() { const form = document.forms[0];
+  const selectAuthor = form.querySelector('input[name="author"]');
+  let selectedAuthor = selectAuthor.value;
+  const selectTitle = form.querySelector('input[name="Title"]');
+  let selectedTitle = selectTitle.value;
+  const selectPages = form.querySelector('input[name="pagesnumber"]');
+  let selectedPages = selectPages.value;
+  const selectHasRead = form.querySelector('input[name="hasread"]');
+  let selectedHasRead= selectHasRead.value;
+  book4 = new Book(selectedTitle, selectedAuthor, selectedPages, selectedHasRead);
+  addBookToLibrary(book4);
+  const books = document.querySelectorAll('.book');
+  books.forEach(book => book.remove());
   displayBookStore();
 }
-
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  myLibraryShadow.push(book);
+  return myLibrary;
+}
+function removeBookFromLibrary(id) {
+  for ( let book of myLibrary ) {
+    if ( book.id == id )  {myLibrary.splice(myLibrary.indexOf(book), 1);
+  document.querySelectorAll(`[data='${id}']`).forEach(book => book.remove());}
+  }
+}
 // Click event "add book" triggers pop-up with form for user to input details. 
 addBookButton.addEventListener('click', () => {document.querySelector('#add-book').style.display='flex'; 
 document.querySelector('.grid').classList.add('greyedout')}
@@ -83,20 +91,5 @@ document.querySelector('.grid').classList.toggle('greyedout');}
 // On submit we send the book details to the library.
 submit.addEventListener('click', addBook);
 
-function addBook() { const form = document.forms[0];
-  const selectAuthor = form.querySelector('input[name="author"]');
-  let selectedAuthor = selectAuthor.value;
-  const selectTitle = form.querySelector('input[name="Title"]');
-  let selectedTitle = selectTitle.value;
-  const selectPages = form.querySelector('input[name="pagesnumber"]');
-  let selectedPages = selectPages.value;
-  const selectHasRead = form.querySelector('input[name="hasread"]');
-  let selectedHasRead= selectHasRead.value;
-  book4 = new Book(selectedAuthor, selectedTitle, selectedPages, selectedHasRead);
-  addBookToLibrary(book4);
-  const books = document.querySelectorAll('.book');
-  books.forEach(book => book.remove());
-  displayBookStore();
-}
 // on start-up: entry-point.
 loadInitialLibrary();
